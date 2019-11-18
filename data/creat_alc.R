89c45a487514954d70f9e77022eb4aae67278448
Mounir <Mounir@DESKTOP-RCUVSAE>
2019-11-18 22:18:27 +0100

    Halfway through data wrangling exercise 3

---
+++ data/creat_alc.R
@@ -0,0 +1,56 @@
+#Mounir Ould Setti 18.11.19
+#Data wrangling for exercise 3: Logistic regression
+#Data source: UCI Machine Learning Repository Student Performance Data https://archive.ics.uci.edu/ml/datasets/Student+Performance
+
+#Importing the data
+mat<-read.csv("data/student-mat.csv", header = TRUE, sep = ";")
+por<-read.csv("data/student-por.csv", header = TRUE, sep = ";")
+
+##exploring the structure and dimensions of the data
+str(mat)
+dim(mat)
+
+str(por)
+dim(por)
+
+#Joining the two datasets
+library(dplyr)
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+##joining the two datasets by the selected identifiers and leaving only rows present in both datasets
+math_por <- inner_join(mat, por, by = join_by,suffix=c(".math",".por"))
+
+str(math_por)
+dim(math_por)
+
+#Combining the duplicated answers
+math <- mat
+
+alc <- select(math_por, one_of(join_by))
+
+## the columns in the datasets which were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+## print out the columns not used for joining
+notjoined_columns
+
+## for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(math_por, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+
+##Checking that everything is alright
+glimpse(alc)
